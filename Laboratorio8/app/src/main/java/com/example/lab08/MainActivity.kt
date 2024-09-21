78468a48899c8c51c0adc56fad3b1eab5b5fd520
SebastianUVG <gar22291@uvg.edu.gt>
2024-09-21 15:30:07 -0600

    .

--- Laboratorio8/app/src/main/java/com/example/lab08/MainActivity.kt
+++ Laboratorio8/app/src/main/java/com/example/lab08/MainActivity.kt
@@ -4,13 +4,22 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import kotlinx.serialization.Serializable
 import com.example.lab08.ui.theme.Lab08Theme
 
 class MainActivity : ComponentActivity() {
@@ -20,14 +29,69 @@ class MainActivity : ComponentActivity() {
         setContent {
             Lab08Theme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                    val navController = rememberNavController()
+                    NavHost(
+                        navController = navController,
+                        startDestination = ScreenADestination,
+                        modifier = Modifier
+                            .fillMaxSize()
+                            .padding(innerPadding)
+                    ) {
+                        composable<ScreenADestination> {
+                            Column (
+                                modifier = Modifier.fillMaxSize(),
+                                horizontalAlignment = Alignment.CenterHorizontally,
+                                verticalArrangement = Arrangement.Center
+                            ){
+                                Text(text = "Screen A",
+                                    style = MaterialTheme.typography.displayLarge)
+                                Button(onClick = { navController.navigate(route = ScreenBDestination) }) {
+                                    Text(text = "Navegar a B")
+
+                                }
+                            }
+
+                        }
+
+
+
+                        composable<ScreenBDestination>{
+                            Column (
+                                modifier = Modifier.fillMaxSize(),
+                                horizontalAlignment = Alignment.CenterHorizontally,
+                                verticalArrangement = Arrangement.Center
+                            ){
+                                Text(text = "Screen B",
+                                    style = MaterialTheme.typography.displayLarge)
+                                Button(onClick = { navController.navigate(route = ScreenADestination) }) {
+                                    Text(text = "Navegar a A")
+
+                                }
+                            }
+
+                        }
+
+                    }
                 }
             }
         }
     }
+}
+
+
+@Serializable
+data object ScreenADestination
+
+@Serializable
+data object ScreenBDestination
+
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
 }
 
 @Composable
